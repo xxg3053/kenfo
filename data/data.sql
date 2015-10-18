e00757458b70d4139e9dfeaeccd078b532309bc7
xxg3053 <xxg3053@qq.com>
2015-10-18 17:58:35 +0800

    change something

---
+++ data/data.sql
@@ -0,0 +1,53 @@
+--管理员
+DROP TABLE IF EXISTS `kenfo_admin`;
+CREATE TABLE `kenfo_admin` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` char(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+--分类
+DROP TABLE IF EXISTS `kenfo_cate`;
+CREATE TABLE `kenfo_cate` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `cName` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cName` (`cName`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--商品
+DROP TABLE IF EXISTS `kenfo_pro`;
+CREATE TABLE `kenfo_pro` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `pName` varchar(255) NOT NULL,
+  `pSn` varchar(50) NOT NULL,
+  `pNum` int(10) unsigned DEFAULT '1',
+  `mPrice` decimal(10,2) NOT NULL,
+  `iPrice` decimal(10,2) NOT NULL,
+  `pDesc` text,
+  `pubTime` int(10) unsigned NOT NULL,
+  `isShow` tinyint(1) DEFAULT '1',
+  `isHot` tinyint(1) DEFAULT '0',
+  `cId` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pName` (`pName`),
+  UNIQUE KEY `pName_2` (`pName`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+--用户
+DROP TABLE IF EXISTS `kenfo_user`;
+CREATE TABLE `kenfo_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` char(32) NOT NULL,
+  `sex` enum('男','女','保密') NOT NULL DEFAULT '保密',
+  `email` varchar(50) NOT NULL,
+  `face` varchar(50) NOT NULL,
+  `regTime` int(10) unsigned NOT NULL,
+  `activeFlag` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
